fe1a70024c16b2028e2467080df74c74c2cc4136
Sunkanmi Otitolaye <leosuky99@gmail.com>
2022-12-01 10:54:44 -0800

    fixed some errors

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,11 +5,4 @@ if __name__ == "__main__":
     for arg in sys.argv:
         if arg != sys.argv[0]:
             result += int(arg)
-    print(result)#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
     print(result)
