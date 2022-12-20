4e73081d4d502cb0b8f7d8c72ca4208c74a32922
Sunkanmi Otitolaye <leosuky99@gmail.com>
2022-12-20 07:37:48 -0800

    fixing task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Define a new class Square"""
+"""
+This is the "Square"  module.
+
+This module provides a simple Square class.
+"""
 
 class Square:
     """Class Square that defines a square."""
